57267515291e5e795ec2354629b769505bb1d80d
Manjil Tamang <lamamanjil@gmail.com>
2023-10-09 02:46:11 +0545

    add github link

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
 import SignoutButton from "./signout-button";
+import Image from "next/image";
 
 export default function Navbar() {
   const pathname = usePathname();
@@ -15,7 +16,7 @@ export default function Navbar() {
         <h1 className="font-bold text-xl">
           <Link href="/">NextAuth</Link>
         </h1>
-        <ul className="flex space-x-2">
+        <ul className="flex space-x-2 items-center -mr-[1rem]">
           {session ? (
             <li>
               <SignoutButton />
@@ -38,6 +39,19 @@ export default function Navbar() {
               ) : null}
             </>
           )}
+          <li>
+            <Link
+              className="px-4 py-2 block"
+              href="https://github.com/manjillama/next-auth-mongo-example"
+            >
+              <Image
+                src={`/images/github-logo.svg`}
+                alt="me"
+                width={20}
+                height={20}
+              />
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
